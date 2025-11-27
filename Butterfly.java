531b1fbc9fe045e2a2226d626b52e7f8975bc12b
Vasanth K <vasanthkamaraj942003@gmail.com>
2025-11-27 11:23:40 +0530

    Refactor condition in Butterfly pattern logic

--- Butterfly.java
+++ Butterfly.java
@@ -6,7 +6,7 @@ class Butterfly
 		{
 			for (int col=1;col<=n*2-1;col++) 
 			{
-				if((row>=col||row+col>=n*2)&&(row<=col||row+col<=n*2))
+				if(row>=col&&row+col<=n*2||row<=col&&row+col>=n*2)
 					System.out.print("*"+" ");
 				else
 					System.out.print(" "+" ");
